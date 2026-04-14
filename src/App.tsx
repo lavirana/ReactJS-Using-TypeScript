6f1f29fc165ba6c9e29b286c1428f2d7d7c3d455
Ashish Rana <ashishrana288@gmail.com>
2026-04-14 05:47:32 +0530

    API Calling in React and Typescript

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,8 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Todo from './components/Todo';
 import Counter from './components/Counter';
+import { stat } from "fs";
+
 
 const myTodoItems = [
     {
@@ -13,11 +15,36 @@ const myTodoItems = [
     },
 ];
 
+
+interface TodoItem {
+    completed: boolean
+    id: number
+    title: string
+    userId: number
+}
+
+
 const App: React.FC = () => {
+    const [state, setSate] = useState<boolean>(true)
+    const [todos, setTodos] = useState<TodoItem[]>([])
+
+    useEffect(() => {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+        .then((response) => response).then(e => e.json()).then(e => setTodos(e as TodoItem[])); 
+    }, [])
+
+
     return (
         <div style={{ border: "1px solid red" }} >
             <Todo items={myTodoItems} />
-            <Counter />
+            <button onClick={e => setSate(!state)} >Toogle</button>
+            {state ? <Counter /> : ""}
+
+            <div>
+                {
+                    todos.map(todo => <li>{todo.title}</li>)
+                }
+            </div>
         </div>
     );
 };
--- src/components/Counter/index.tsx
+++ src/components/Counter/index.tsx
@@ -1,8 +1,19 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 const Counter: React.FC = (props) => {
    //let counter = 0;
     let [counter, setValue] = useState(0);
+
+    useEffect(() => {
+        console.log("Counter Mounted");
+        return function(){
+            console.log('Unmount counter');
+        }
+    }, []);
+    
+    useEffect(() => {
+        console.log("user updated count");
+    }, [counter]); 
 
     const handleDecrement = () => {
         if(counter === 0) return;
